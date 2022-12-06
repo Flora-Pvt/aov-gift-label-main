00f02f28ec42dc1bb7f542310efc9d3763aa0ca3
Flora Preuvot <flora@qomon.com>
2022-12-06 09:35:47 +0100

    add data binding feature

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,40 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
           </div>
         </div>
       </div>
+
+      <label for="receiver">To:</label>
+      <input v-model="to" id="receiver" />
+
+      <label for="author">From:</label>
+      <input v-model="from" id="author" />
     </div>
   </div>
 </template>
 
-<script setup>
-// ...
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const to = ref('')
+const from = ref('')
 </script>
+
+<style>
+label {
+  display: block;
+  margin-top: 8px;
+}
+
+input {
+  width: 100%;
+  border: gray 1px solid;
+  border-radius: 8px;
+  background-color: snow;
+  height: 40px;
+  padding: 2px 8px;
+}
+</style>
